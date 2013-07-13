83bfefe40ec49255b389271f640180670e4bf4ec
root <christopher.e.moore@gmail.com>
2013-07-13 12:04:03 +0000

    first commit

---
+++ common.lua
@@ -0,0 +1,78 @@
+--[[
+	Copyright (c) 2009-2013 Christopher E. Moore ( christopher.e.moore@gmail.com / http://christopheremoore.net )
+
+	Permission is hereby granted, free of charge, to any person obtaining a copy
+	of this software and associated documentation files (the "Software"), to deal
+	in the Software without restriction, including without limitation the rights
+	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+	copies of the Software, and to permit persons to whom the Software is
+	furnished to do so, subject to the following conditions:
+
+	The above copyright notice and this permission notice shall be included in
+	all copies or substantial portions of the Software.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+	THE SOFTWARE.
+--]]
+
+-- TODO -move to htmlparser and make instances of the tree nodes
+
+function findnode(list, callback)
+	local res = {}
+	assert(type(list) == 'table')
+	for _,v in ipairs(list) do
+		if callback(v) then
+			table.insert(res, v)
+		end
+	end
+	return res
+end
+
+function findtags(list, tagname, attrs)
+	return findnode(list, function(n)
+		if type(n) ~= 'table' then return false end
+		if n.tag ~= tagname then return false end
+		if attrs then
+			for k,v in pairs(attrs) do
+				local found = false
+				if n.attrs then
+					for _,kv in ipairs(n.attrs) do
+						if kv.name == k then
+							if kv.value ~= v then return false end
+							found = true
+						end
+					end
+				end
+				if not found then return false end
+			end
+		end
+		return true
+	end)
+end
+
+function findtag(...)
+	return (findtags(...))[1]
+end
+
+function findchild(node, ...)
+	return findtag(node.child, ...)
+end
+
+function findchilds(node, ...)
+	return findtags(node.child, ...)
+end
+
+function findattr(node, name)
+	if node.attrs then
+		for _,kv in ipairs(node.attrs) do
+			if kv.name == name then return kv.value end
+		end
+	end
+end
+
+
---
+++ tests/prettyprint.lua
@@ -0,0 +1,61 @@
+--[[
+	Copyright (c) 2009-2013 Christopher E. Moore ( christopher.e.moore@gmail.com / http://christopheremoore.net )
+
+	Permission is hereby granted, free of charge, to any person obtaining a copy
+	of this software and associated documentation files (the "Software"), to deal
+	in the Software without restriction, including without limitation the rights
+	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+	copies of the Software, and to permit persons to whom the Software is
+	furnished to do so, subject to the following conditions:
+
+	The above copyright notice and this permission notice shall be included in
+	all copies or substantial portions of the Software.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+	THE SOFTWARE.
+--]]
+
+args = {...}
+
+local page
+if #args > 0 then
+	local filename = args[1]
+	local file = assert(io.open(filename, 'r'))
+	page = file:read('*a')
+	file:close()
+else
+	require 'socket'
+	local http = require 'socket.http'
+
+	local mainpage = 'http://christopheremoore.net'
+	do
+		local filename = 'cachedpage.html'
+		local file = io.open(filename, 'r')
+		if file then
+			page = assert(file:read('*a'))
+			file:close()
+		else
+			page = assert(http.request(mainpage))
+			file = assert(io.open(filename, 'w'))
+			file:write(page)
+			file:close()
+		end
+	end
+end
+
+-- [[
+require 'htmlparser'
+local tree = htmlparser.new(page):parse()
+htmlparser.prettyprint(tree)
+--Parser.debugprint(tree)
+--]]
+
+--[[
+filename = 'cachedpage.html'
+require 'old.autoformat'
+--]]
